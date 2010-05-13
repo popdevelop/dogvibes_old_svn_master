433268d9e1b5188abf2291bce68e5b3aacf7307f
Johan Brissmyr <brissmyr@gmail.com>
2010-05-13 20:26:43 +0000

    Correct album protocol

--- dogvibes/src/spotifysource.py
+++ dogvibes/src/spotifysource.py
@@ -105,7 +105,7 @@ class SpotifySource:
             track['title'] = e.find('.//{%s}name' % ns).text
             track['artist'] = e.find('.//{%s}artist/{%s}name' % (ns, ns)).text
             track['album'] = e.find('.//{%s}album/{%s}name' % (ns, ns)).text
-            track['album_uri'] = tree.find('.//{%s}album' % ns).attrib['href']
+            track['album_uri'] = "spotify://" + tree.find('.//{%s}album' % ns).attrib['href']
             track['duration'] = int(float(e.find('.//{%s}length' % ns).text) * 1000)
             track['uri'] = "spotify://" + e.items()[0][1]
             track['popularity'] = e.find('.//{%s}popularity' % ns).text
