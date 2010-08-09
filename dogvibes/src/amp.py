976e6e93e26a17f39591d53490b056c5ec0f4e90
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2010-08-09 18:47:42 +0000

    Update vote version when track is changed from EOF from pipeline.

--- dogvibes/src/amp.py
+++ dogvibes/src/amp.py
@@ -193,6 +193,7 @@ class Amp():
             self.next_track()
             #request.push({'state': self.get_state()})
             #request.push(self.track_to_client())
+            self.vote_version += 1
             self.needs_push_update = True
             # TODO: is this enough? An update is pushed to the clients
             # but will the info be correct?
